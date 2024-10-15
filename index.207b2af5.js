4e6c83e5d4db1d48ad4613b9035aae2d58ce6170
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-15 19:34:50 +0300

    make build

---
+++ index.207b2af5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.getBoundingClientRect(),n=c.width,i=c.height,l=t.getBoundingClientRect(),o=l.width,d=l.height;t.style.left="".concat((n-o)/2,"px"),t.style.top="".concat((i-d)/2,"px");
+//# sourceMappingURL=index.207b2af5.js.map
